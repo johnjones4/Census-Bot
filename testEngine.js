56a65342466ea651020261ed326d2ea9da13f06d
John Jones <johnj@casefoundation.org>
2017-07-31 17:12:41 -0400

    parser updates and command line updates

--- testEngine.js
+++ testEngine.js
@@ -1,13 +1,41 @@
 const botEngine = require('./lib/botEngine');
 const database = require('./lib/database');
+const readline = require('readline');
+
+const exitStrings = [
+  'q',
+  'quit',
+  'exit',
+  'end',
+  'cancel'
+];
 
 database.init()
   .then(() => {
-    return botEngine.processStatement(process.argv[2]);
-  })
-  .then((response) => {
-    console.log(response);
-    process.exit(0);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    const askQuestion = () => {
+      rl.question('> ',(answer) => {
+        rl.pause();
+        if (exitStrings.indexOf(answer.toLowerCase()) >= 0) {
+          rl.close();
+          process.exit(0);
+        } else {
+          botEngine.processStatement(answer)
+            .then((response) => {
+              console.log(response);
+              askQuestion();
+            })
+            .catch((err) => {
+              console.error(err);
+              askQuestion();
+            });
+        }
+      });
+    }
+    askQuestion();
   })
   .catch((err) => {
     console.error(err);
